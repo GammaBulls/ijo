fc611ea2ad429e7550392b14208a0253a273d086
CookieMichal <mrcookiemichal@gmail.com>
2020-01-13 16:19:03 +0100

    WIP: Ad component

--- frontend/src/modules/Ad/Ad.js
+++ frontend/src/modules/Ad/Ad.js
@@ -1,7 +1,45 @@
+import { formatDistanceToNow } from "date-fns";
+import { pl } from "date-fns/locale";
 import React from "react";
+import { useParams } from "react-router";
+import useGetAd from "../../services/useGetAd";
+import DefaultLayout from "../shared/layouts/DefaultLayout";
+import { ContentSection } from "./Ad.components";
 
 const Ad = () => {
-  return <h1>Ad</h1>;
+  const { id } = useParams();
+  const { data, error, loading } = useGetAd({ id });
+
+  // return JSON.stringify({ data, error, loading });
+
+  if (loading) {
+    return "Loading...";
+  }
+
+  if (error) {
+    return `Error: ${error}`;
+  }
+
+  const { title, description, start_date, price } = data;
+
+  const formattedDate = formatDistanceToNow(new Date(start_date), {
+    locale: pl,
+    addSuffix: true,
+  });
+
+  // return JSON.stringify(data);
+
+  return (
+    <DefaultLayout>
+      <ContentSection>
+        Ogloszenie {id}
+        <br />
+        {title} - {formattedDate} ({price} PLN)
+        <br />
+        {description}
+      </ContentSection>
+    </DefaultLayout>
+  );
 };
 
 export default Ad;
