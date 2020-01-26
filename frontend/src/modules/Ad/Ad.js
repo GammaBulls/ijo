547510ddb28456804fd4a1aea265d19446aef4da
CookieMichal <mrcookiemichal@gmail.com>
2020-01-26 19:11:47 +0100

    Hit.jpg

--- frontend/src/modules/Ad/Ad.js
+++ frontend/src/modules/Ad/Ad.js
@@ -1,16 +1,28 @@
 import { formatDistanceToNow } from "date-fns";
 import { pl } from "date-fns/locale";
-import React from "react";
+import React, { useMemo } from "react";
 import { useParams } from "react-router";
 import useGetAd from "../../services/Ads/useGetAd";
 import DefaultLayout from "../shared/layouts/DefaultLayout";
 import { ContentSection } from "./Ad.components";
+import useGetCategories from "../../services/useGetCategories";
+import useCategoriesOptions from "../shared/hooks/useCategoriesOptions";
+import useGetAdPhotos from "../../services/Ads/useGetAdPhotos";
 
 const Ad = () => {
   const { id } = useParams();
   const { data, error, loading } = useGetAd({ id });
+  const [categories] = useCategoriesOptions();
 
-  // return JSON.stringify({ data, error, loading });
+  const { data: photoData } = useGetAdPhotos({ id: id });
+
+  // eslint-disable-next-line no-unused-vars
+  const photos = useMemo(() => {
+    if (!Array.isArray(photoData)) {
+      return [];
+    }
+    return photoData.map(v => v.link);
+  }, [photoData]);
 
   if (loading) {
     return "Loading...";
@@ -21,21 +33,25 @@ const Ad = () => {
   }
 
   const { title, description, start_date, price } = data;
+  const category = categories.find(v => v.value === data.category) || {};
 
   const formattedDate = formatDistanceToNow(new Date(start_date), {
     locale: pl,
     addSuffix: true,
   });
 
-  // return JSON.stringify(data);
-
   return (
     <DefaultLayout>
       <ContentSection>
-        Ogloszenie {id}
+        <h2>{title}</h2>
+        <span>Kategoria: {category.label}</span>
+        <span>Utworzono: {formattedDate}</span>
+        <span>Cena: {price} PLN</span>
         <br />
-        {title} - {formattedDate} ({price} PLN)
-        <br />
+        {photos.map(link => (
+          <img src={link} key={link} />
+        ))}
+        <h4>Opis:</h4>
         {description}
       </ContentSection>
     </DefaultLayout>
